7317a3a00b0ebc0399830d0615b14422a0b87bdf
keshavsharma5118 <keshav.s5118@gmail.com>
2025-01-23 02:56:31 +0530

    changed the port 5000->8080

--- server.js
+++ server.js
@@ -78,5 +78,5 @@ app.get("/image/:rollNumber", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
